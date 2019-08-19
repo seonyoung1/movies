44f686719fd04458ca72cacb648c6762670dca83
김선영 <tjsgur35@betree.co.kr>
2019-08-20 02:24:55 +0900

    home 함수형 복원

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -61,6 +61,56 @@ class HomeContainer extends Component {
 }
 
 
+// const HomeContainer = ({ page, SetActions }) => {
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [error, setError] = useState(null);
+//     const [movies, setMovies] = useState([]);
+//
+//     useEffect(() => {
+//         nowPlayingList();
+//
+//         window.addEventListener("scroll", handleScroll);
+//         return () => window.removeEventListener("scroll", handleScroll);
+//     }, []);
+//
+//     const nowPlayingList = async () => {
+//         try {
+//             const { data: { results : result }} = await moviesApi.nowPlaying(page);
+//             setMovies([...movies, ...result]);
+//         } catch {
+//             setError("영화 목록을 가져오는데 실패했습니다.")
+//         } finally {
+//             setIsLoading(false);
+//         }
+//     };
+//
+//     const handleScroll = () => {
+//         let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+//         let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+//         let clientHeight = document.documentElement.clientHeight + 100 ;
+//         if( scrollTop + clientHeight > scrollHeight ){
+//             SetActions.pageSet(page + 1);
+//             //console.log(`scroll : ${number}`);
+//             nowPlayingList();
+//         }
+//     };
+//
+//     const pageSet = number => {
+//         SetActions.pageSet(number);
+//     };
+//
+//     return (
+//         <Home
+//             page={page}
+//             movies={movies}
+//             isLoading={isLoading}
+//             error={error}
+//             pageSet={pageSet}
+//         />
+//     );
+// };
+
+
 const mapStateToProps = ({setting}) => ({
     page: setting.page,
     contents: setting.contents,
