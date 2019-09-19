cf7da17cbea1fdfbee141e9734f630f1568b85f1
김선영 <tjsgur35@betree.co.kr>
2019-09-19 21:03:42 +0900

    gallery

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.p`
     width:100%;margin:50px 0;
     display:flex;
     justify-content: center;
