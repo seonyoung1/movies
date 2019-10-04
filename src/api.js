91d216581bd08094ef8d5ed014909f3c1fd9c74b
김선영 <tjsgur35@betree.co.kr>
2019-10-04 11:43:44 +0900

    언어변경

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params: {
         api_key: "6f26edc26d6ed9d164d9bc332258be3f",
-        language: "en-US"
+        language: "ko"
     }
 });
 
@@ -22,7 +22,7 @@ export const moviesApi = {
     search: term => api.get("search/movie",{
         params: {
             // api_key: "6f26edc26d6ed9d164d9bc332258be3f",
-            query: encodeURI(term)
+            query: term
         }
     }),
     movieDetail: id => api.get(`movie/${id}&append_to_response=videos`, {
